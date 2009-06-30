aac58fd2c835761983e62c189fd55629cb05df1d
root <pepe5@email.cz>
2009-06-30 14:00:55 -0400

    min_by intro

--- merge-view.rb
+++ merge-view.rb
@@ -1,13 +1,19 @@
 # = Merge View pkg
+class HashPlus < Hash
+  def min_by ();
+        # puts @each .keys {|i| }
+  end
+end
+
 class MoreSrcsFile #?<< File
   def m1 (kvp={}); p = {:k1=>'d1', :k2=>'d2'} .merge! kvp; puts "p:#{p.inspect}"; end
   def initialize (fds, modeString='r')
-    @cache = {}
+    @cache = HashPlus .new
     @srcs = fds .collect {|i| File .new i, modeString}; end
   def readall (); @srcs .collect {|f| f .readlines}; end
   def readline ();
-    if @cache .size < 1 then @srcs .each {|f| @cache[f.path] = f .readline} end
-    o = @cache
+    if @cache .size < 1 then @srcs .each {|f| @cache[f.path] = [f .readline]} end
+    o = @cache [(@cache .keys) [1]] .pop
     o; end
 end
 
