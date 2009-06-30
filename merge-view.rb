757cdb15662e68b902d3540aa084d4032399a121
root <pepe5@email.cz>
2009-06-30 12:53:32 -0400

    readall collecting

--- merge-view.rb
+++ merge-view.rb
@@ -1,8 +1,8 @@
 # = Merge View pkg
 class MoreSrcsFile #?<< File
   def m1 (kvp={}); p = {:k1=>'d1', :k2=>'d2'} .merge! kvp; puts "p:#{p.inspect}"; end
-  def initialize (fds, modeString='r'); @srcs = fds .collect do |i| f = File .new i, modeString; end; end
-  def readall (); for f in @srcs do f .readlines; end; end
+  def initialize (fds, modeString='r'); @srcs = fds .collect do |i| File .new i, modeString; end; end
+  def readall (); @srcs .collect do |f| f .readlines; end; end
 end
 
-puts ((myFiles = MoreSrcsFile .new ARGV) .inspect) #.readall
+puts ((myFiles = MoreSrcsFile .new ARGV) .readall)
