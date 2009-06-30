34d62e7bdd9337be98fa3d50f8fae821b73ae7e7
root <pepe5@email.cz>
2009-06-30 16:14:55 -0400

    fillup start

--- merge-view.rb
+++ merge-view.rb
@@ -1,14 +1,20 @@
 # = Merge View pkg
+#!< HashAsoc < Hash; def key...; def value...; end
 class HashPlus < Hash
   def min ()
     min = {(self.keys) [0] => self [(self.keys) [0]] [0]}
-    p min
+    # p min
     for k in self.keys do
-      puts "#{k.inspect}: #{self [k][0] .inspect}"
+      # puts "#{k.inspect}: #{self [k][0] .inspect}"
       if ((min[(min .keys)[0]] .scan /\d+/) [0] .to_i) > ((self[k][0] .scan /\d+/) [0] .to_i) then
         min = {k=>self[k]}
       end; end
     min; end
+  def fillupFrom (srcs);
+    for k,v in self do
+      if v.size < 1 then self[k] << (srcs .find {|f| f.path==k}) .readline end
+    end
+  end
 end
 
 class MoreSrcsFile #?<< File
@@ -16,16 +22,19 @@ class MoreSrcsFile #?<< File
   attr :cache, :srcs
   def initialize (fds, modeString='r')
     @cache = HashPlus .new
-    @srcs = fds .collect {|i| File .new i, modeString}; end
-  def readall (); @srcs .collect {|f| f .readlines}; end
+    @srcs = fds .collect {|i| File .new i, modeString} end
+  def readall (); @srcs .collect {|f| f .readlines} end
   def readline ();
     if @cache .size < 1 then @srcs .each {|f| @cache[f.path] = [f .readline]} end
     minCons = @cache .min
-    o = @cache [(minCons .keys)[0]] .pop
+    o = @cache [(minCons .keys)[0]] .pop #&
+    @cache .fillupFrom @srcs
     o; end
 end
 
 puts ((myFiles = MoreSrcsFile .new ARGV) .readline)
-
+puts "cache: #{myFiles.cache .inspect}"
+puts ((myFiles = MoreSrcsFile .new ARGV) .readline)
+puts "cache: #{myFiles.cache .inspect}"
 # puts "min (_by..):"
 # myFiles .cache .min
