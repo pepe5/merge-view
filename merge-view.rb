f8faabc4384b1fa92bdd38e42f33ad65210a3579
root <pepe5@email.cz>
2009-06-30 13:39:43 -0400

    readline reads

--- merge-view.rb
+++ merge-view.rb
@@ -1,8 +1,14 @@
 # = Merge View pkg
 class MoreSrcsFile #?<< File
   def m1 (kvp={}); p = {:k1=>'d1', :k2=>'d2'} .merge! kvp; puts "p:#{p.inspect}"; end
-  def initialize (fds, modeString='r'); @srcs = fds .collect do |i| File .new i, modeString; end; end
-  def readall (); @srcs .collect do |f| f .readlines; end; end
+  def initialize (fds, modeString='r')
+    @cache = {}
+    @srcs = fds .collect {|i| File .new i, modeString}; end
+  def readall (); @srcs .collect {|f| f .readlines}; end
+  def readline ();
+    if @cache .size < 1 then @srcs .each {|f| @cache[f.path] = f .readline} end
+    o = @cache
+    o; end
 end
 
-puts ((myFiles = MoreSrcsFile .new ARGV) .readall)
+puts ((myFiles = MoreSrcsFile .new ARGV) .readline)
